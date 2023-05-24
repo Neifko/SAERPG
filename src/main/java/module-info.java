9c9186c924342402c563d7d50310244af1718ceb
Victor G <20vg01@gmail.com>
2023-05-24 11:01:11 +0200

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.hsn.saerpgihm {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.hsn.saerpgihm to javafx.fxml;
+    exports com.hsn.saerpgihm;
+    exports model;
+    exports iofile;
+    exports client;
+}
