e5b61b752ad8a86c1c17f215a5fbd18ed3e451ac
Ostap <ostap.tymchyshyn.92@gmail.com>
2023-05-30 16:32:39 +0200

    finalisation de la méthode exhaustive() + ajout des commentaires des lignes sur la méthode exhaustive() + ajoute de commentaires des autres méthodes

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -15,6 +15,11 @@ public class Player {
 
     private ArrayList<String> states;
 
+    /***
+     * Constructeur de la classe Player.
+     * Initialise les attributs du joueur en fonction du scénario fourni.
+     * @param parScenario Le scénario du jeu.
+     */
     public Player(Scenario parScenario) {
         scenario = parScenario;
         todoQuests = new ArrayList<>();
@@ -23,7 +28,6 @@ public class Player {
         precondCompleted = new ArrayList<>();
         states = new ArrayList<>();
     }
-
     /**
      * Solution efficace correspond au joueur qui realise les quetes les plus proches
      */
@@ -77,56 +81,60 @@ public class Player {
      * Solution exhaustive correspond au joueur qui realise toutes les quetes
      */
     public void exhaustive() {
+        // Liste des quêtes disponibles
         ArrayList<Quest> availableQuests = new ArrayList<>(scenario.getProvQuests());
+        // Liste des quêtes complétées
         ArrayList<Quest> completedQuests = new ArrayList<>();
-        ArrayList<Quest> postponedQuests = new ArrayList<>();
-
+        // Recherche de la quête 0
+        Quest questZero = null;
+        for (Quest quest : availableQuests) {
+            if (quest.getId() == 0) {
+                questZero = quest;
+                break;
+            }
+        }
+        // Suppression de la quête 0 des quêtes disponibles si elle existe
+        if (questZero != null) {
+            availableQuests.remove(questZero);
+        }
+        // Traitement des quêtes disponibles
         while (!availableQuests.isEmpty()) {
+            // Recherche de la quête la plus proche et réalisable
             Quest closestQuest = null;
             int distMin = Integer.MAX_VALUE;
-
             for (Quest quest : availableQuests) {
-                if (quest.getId() != 0 && (quest.noPrecond() || quest.hasCompletedPrecond(precondCompleted))) {
+                // Vérification des préconditions de la quête
+                if (quest.noPrecond() || quest.hasCompletedPrecond(precondCompleted)) {
                     int dist = calculDistance(quest.getCoordinates());
+                    // Mise à jour de la quête la plus proche si la distance est inférieure à la distance minimale
                     if (dist < distMin) {
                         distMin = dist;
                         closestQuest = quest;
                     }
-                }
-            }
-            if (closestQuest == null) {
-                closestQuest = availableQuests.get(0);
-            }
-            playerCoord = closestQuest.getCoordinates();
-            xp += closestQuest.getExperience();
-            duration += closestQuest.getDuration();
-            completedQuests.add(closestQuest);
-            availableQuests.remove(closestQuest);
-
-            Iterator<Quest> iterator = postponedQuests.iterator();
-            while (iterator.hasNext()) {
-                Quest postponedQuest = iterator.next();
-                boolean canCompletePostponed = true;
-                for (int precondition : postponedQuest.getPreconditions()) {
-                    if (!completedQuests.contains(precondition)) {
-                        canCompletePostponed = false;
-                        break;
+                    // Si la distance est égale à la distance minimale, on choisit la quête avec un ID inférieur
+                    else if (dist == distMin && quest.getId() < closestQuest.getId()) {
+                        closestQuest = quest;
                     }
                 }
-                if (canCompletePostponed) {
-                    iterator.remove();
-                    availableQuests.add(postponedQuest);
-                }
+            }
+            // Réalisation de la quête la plus proche
+            if (closestQuest != null) {
+                move(closestQuest.getCoordinates());
+                doQuest(closestQuest);
+                completedQuests.add(closestQuest);
+                availableQuests.remove(closestQuest);
             }
         }
+        // Ajout de la quête 0 en dernière position si elle existe
+        if (questZero != null) {
+            completedQuests.add(questZero);
+        }
+        // Affichage des quêtes complétées
         System.out.println("Quêtes Complétées :");
-        completedQuests.sort(Comparator.comparingInt(q -> Arrays.hashCode(q.getPreconditions())));
         for (Quest quest : completedQuests) {
             System.out.println(quest);
         }
     }
-
-
     /**
      * calcule la distance entre les coordonnées du joueur et les coordonnées de la quete et la renvoie par la suite
      * @return float
@@ -134,21 +142,26 @@ public class Player {
      */
     private int calculDistance(int[] questCoord) {
         int[] playerCoord = this.playerCoord;
-
+        // Calcul des différences de coordonnées en X et en Y
         int diffCoordX = questCoord[0] - playerCoord[0];
         int diffCoordY = questCoord[1] - playerCoord[1];
-
-        // (int) Math.sqrt(Math.pow(playerCoord[0]-questCoord[0],2) + Math.pow(playerCoord[1], questCoord[1]))
+        // Calcul de la distance en utilisant la distance de Manhattan (somme des différences absolues)
         return Math.abs(diffCoordX) + Math.abs(diffCoordY);
     }
 
+    /***
+     * Méthode pour afficher l'état actuel du joueur
+     */
     public void showState() {
         for (String log : states){
             System.out.println(log);
         }
     }
 
-
+    /***
+     * Permet d'obtenir une représentation textuelle de l'objet joueur
+     * @return Une chaîne de caractères représentant le joueur ("Player 0")
+     */
     public String toString(){
         return "Player 0";
     }
