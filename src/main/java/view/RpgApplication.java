11bf0fe290e3b2f16e20f52bfff6f91e396e4523
Ostap <ostap.tymchyshyn.92@gmail.com>
2023-05-31 22:39:34 +0200

    ajout des classes RpgApplication.java ,et  MainController.java ainsi que du fichier main.fxml , qui vont permettre de créer la première structure de l'IHM , cependant ce n'est que provisoire pour pouvoir voir de plus près, le code va être changé drastiquement

--- src/main/java/view/RpgApplication.java
+++ src/main/java/view/RpgApplication.java
@@ -1,6 +1,25 @@
 package view;
 
-public class RpgApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    // copie colle PlanningApplication
+import java.io.IOException;
+
+public class RpgApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        primaryStage.setTitle("RPG Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
