75072439800e8ec2d0a628c81703deda3927c757
Ostap <ostap.tymchyshyn.92@gmail.com>
2023-06-05 18:46:09 +0200

    séparation du code RpgApplication.java , la partie avec GridPane a été déplacé dans la NOUVELLE classe GridPaneRoot.java désormais RpgApplication.java ne fait que le travaille nécéssaire celui de lancement de gridpane et scene et la lecture du css

---
+++ src/main/java/view/GridPaneRoot.java
@@ -0,0 +1,83 @@
+package view;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import java.io.File;
+
+public class GridPaneRoot extends GridPane {
+    public GridPaneRoot() {
+        setPadding(new Insets(10));
+        setHgap(10);
+        setVgap(10);
+
+        Label titleLabel = new Label("Sélectionnez un scénario :");
+        add(titleLabel, 0, 0);
+
+        ComboBox<String> scenarioComboBox = new ComboBox<>();
+        add(scenarioComboBox, 0, 1);
+
+        // Charger les scénarios disponibles
+        loadScenarios(scenarioComboBox);
+
+        Label coordinatesLabel = new Label("Sélectionnez les coordonnées :");
+        add(coordinatesLabel, 0, 2);
+
+        Label xLabel = new Label("x :");
+        add(xLabel, 0, 3);
+
+        TextField xTextField = new TextField("0");
+        add(xTextField, 1, 3);
+
+        Label yLabel = new Label("y :");
+        add(yLabel, 0, 4);
+
+        TextField yTextField = new TextField("0");
+        add(yTextField, 1, 4);
+
+        Button generateButton = new Button("Générer");
+        generateButton.setOnAction(event -> {
+            String selectedScenario = scenarioComboBox.getValue();
+            if (selectedScenario != null) {
+                int x = Integer.parseInt(xTextField.getText());
+                int y = Integer.parseInt(yTextField.getText());
+                // Appeler la méthode pour générer les tableaux avec le scénario sélectionné
+                // TODO: Ajouter le code pour générer les tableaux
+                generateSolutions(selectedScenario, x, y);
+            }
+        });
+        add(generateButton, 0, 5);
+    }
+
+    private void loadScenarios(ComboBox<String> scenarioComboBox) {
+        File scenariosDirectory = new File("scenarios");
+        File[] files = scenariosDirectory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    scenarioComboBox.getItems().add(file.getName());
+                }
+            }
+        }
+
+        // Définir le scénario par défaut
+        if (!scenarioComboBox.getItems().isEmpty()) {
+            scenarioComboBox.setValue(scenarioComboBox.getItems().get(0));
+        }
+    }
+
+
+    private void generateSolutions(String selectedScenario, int x, int y) {
+        // Code pour générer les tableaux de solutions efficace et exhaustive
+        // en utilisant le scénario sélectionné et les coordonnées x et y
+
+        // Exemple de code pour afficher les valeurs sélectionnées
+        System.out.println("Scénario sélectionné : " + selectedScenario);
+        System.out.println("Coordonnées x : " + x);
+        System.out.println("Coordonnées y : " + y);
+    }
+}
